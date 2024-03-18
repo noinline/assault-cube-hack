6af3bdb90510c7f7e6a514f0bbd7829ff5d8257a
noinline <139589029+noinline@users.noreply.github.com>
2024-03-18 15:22:26 +0700

    balls

--- Universal OpenGL 2 Kiero Hook/dllmain.cpp
+++ Universal OpenGL 2 Kiero Hook/dllmain.cpp
@@ -1,9 +1,14 @@
 #include "include/imgui/imgui.h"
 #include "include/imgui_hook.h"
 #include "include/kiero/kiero.h"
+#include "vector.h"
 
 #include <Windows.h>
 #include <string>
+
+#define CAMERA_MODIFIED		1852730990
+#define DEBUG_MODE_MODIFIED 262144
+#define HEALTH_MODIFIED		9999
 
 struct offsets_t
 {
@@ -12,6 +17,7 @@ struct offsets_t
 	uintptr_t local_player = 0x18AC00;
 	uintptr_t entity_list = 0x18AC04;
 	uintptr_t player_count = 0x18AC0C;
+	uintptr_t view_matrix = 0x17DFD0;
 } offset;
 
 struct functions_t
@@ -21,7 +27,66 @@ struct functions_t
 	bool funny_camera = false;
 	bool teleport = false;
 	bool noclip = false;
+	bool line = false;
 } func;
+
+struct colors_t
+{
+	ImColor line = (255, 255, 255, 255);
+}col;
+
+static auto
+WorldToScreen(Vector pos) -> Vector2D
+{
+	ImGui::Text("called WorldToScreen");
+	Vector clip = {0.0f, 0.0f, 0.0f};
+	Vector2D ndc = {0.0f, 0.0f};
+	Vector2D result = {0.0f, 0.0f};
+	Vector2D base = {0.0f, 0.0f};
+
+	float matrix[16]{};
+	for (int i = 0; i < 16; i++)
+	{
+		float elemenet_of_matrix =
+			*(float *) (offset.base + offset.view_matrix + 4 * i);
+		matrix[i] = elemenet_of_matrix;
+	}
+
+	clip.z =
+		pos.x * matrix[3] + pos.y * matrix[7] + pos.z * matrix[11] + matrix[15];
+	if (clip.z < 0.2)
+		return base;
+	clip.x =
+		pos.x * matrix[0] + pos.y * matrix[4] + pos.z * matrix[8] + matrix[12];
+	clip.y =
+		pos.x * matrix[1] + pos.y * matrix[5] + pos.z * matrix[9] + matrix[13];
+
+	ndc.x = clip.x / clip.z;
+	ndc.y = clip.y / clip.z;
+
+	result.x = ((ImGui::GetIO().DisplaySize.x / 2 * ndc.x) +
+				(ndc.x + ImGui::GetIO().DisplaySize.x / 2));
+	result.y = (-(ImGui::GetIO().DisplaySize.y / 2 * ndc.y) +
+				(ndc.y + ImGui::GetIO().DisplaySize.y / 2));
+	ImGui::Text("Returned WorldToScreen");
+	return result;
+}
+
+auto
+RenderLineToPlayer(Vector pos, ImColor col, float thickness) -> void
+{
+	pos = {pos.x, pos.y, pos.z};
+
+	Vector2D translate = WorldToScreen(pos);
+
+	if (translate.x == NULL && translate.y == NULL)
+		return;
+	
+	ImGui::GetBackgroundDrawList()->AddLine(ImVec2
+		(ImGui::GetIO().DisplaySize.x / 2, ImGui::GetIO().DisplaySize.y),
+		ImVec2(translate.x, translate.y), col, thickness);
+
+}
 
 auto
 Loop(void) -> void
@@ -40,22 +105,22 @@ Loop(void) -> void
 	int local_player_health_address = local_player + 0xEC;
 	int local_player_health =
 		*reinterpret_cast<int *>(local_player_health_address);
-	const int local_player_health_modified = 9999;
+	const int local_player_health_modified = HEALTH_MODIFIED;
 
 	int camera_address = local_player + 0x324;
 	int camera = *reinterpret_cast<int *>(camera_address);
-	const int camera_modified = 1852730990;
+	const int camera_modified = CAMERA_MODIFIED;
 
 	int debug_mode_free_camera_address = local_player + 0x74;
 	int debug_mode = *reinterpret_cast<int *>(debug_mode_free_camera_address);
-	const int debug_mode_modified = 262144;
+	const int debug_mode_modified = DEBUG_MODE_MODIFIED;
 
 	if (func.godmode)
 	{
 		*reinterpret_cast<int *>(local_player_health_address) =
 			local_player_health_modified;
 	}
-	else if (!func.godmode && local_player_health == 9999)
+	else if (!func.godmode && local_player_health == HEALTH_MODIFIED)
 	{
 		*reinterpret_cast<int *>(local_player_health_address) = 100;
 	}
@@ -64,35 +129,39 @@ Loop(void) -> void
 	{
 		*reinterpret_cast<int *>(camera_address) = camera_modified;
 	}
-	else if (!func.funny_camera && camera == 1852730990)
+	else if (!func.funny_camera && camera == CAMERA_MODIFIED)
 	{
-		*reinterpret_cast<int *>(camera_address) = 0;
+		*reinterpret_cast<int *>(camera_address) = NULL;
 	}
 
 	if (func.noclip)
 	{
-		if (::GetAsyncKeyState(0x06) & 1)
+		if (::GetAsyncKeyState(VK_XBUTTON2) & 1)
 		{
 			*reinterpret_cast<int *>(debug_mode_free_camera_address) =
 				debug_mode_modified;
 		}
 
-		if (::GetAsyncKeyState(0x05) & 1 && debug_mode == 262144)
+		if (::GetAsyncKeyState(VK_XBUTTON1) & 1 &&
+			debug_mode == DEBUG_MODE_MODIFIED)
 		{
-			*reinterpret_cast<int *>(debug_mode_free_camera_address) = 0;
+			*reinterpret_cast<int *>(debug_mode_free_camera_address) = NULL;
 		}
 	}
 
 	int local_player_team_id_address = local_player + 0x30C;
-
 	for (int i = 0; i < player_count; ++i)
 	{
 		int player = *reinterpret_cast<int *>((ent_list + 0x4 * i));
 
+		float player_pos_x = *reinterpret_cast<float *>(player + 0x28);
+		float player_pos_y = *reinterpret_cast<float *>(player + 0x2C);
+		float player_pos_z = *reinterpret_cast<float *>(player + 0x30); 
+
 		int player_team_id =
 			*reinterpret_cast<int *>((ent_list + 0x30C + 0x4 * i));
 
-		int local_player_team_id =	
+		int local_player_team_id =
 			*reinterpret_cast<int *>(local_player_team_id_address);
 
 		if (!player)
@@ -104,13 +173,17 @@ Loop(void) -> void
 		if (!local_player_team_id)
 			continue;
 
-		if (func.teleport && player_team_id != local_player_team_id)
+		if (func.line)
+		RenderLineToPlayer({player_pos_x, player_pos_y, player_pos_z},
+						   col.line, 2.f);
+
+		if (func.teleport)
 		{
 			if (::GetAsyncKeyState(0x54))
 			{
-				*reinterpret_cast<float *>((player + 0x28)) = final_pos_x;
-				*reinterpret_cast<float *>((player + 0x2C)) = local_pos_y;
-				*reinterpret_cast<float *>((player + 0x30)) = local_pos_z;
+				player_pos_x = final_pos_x;
+				player_pos_y = local_pos_y;
+				player_pos_z = local_pos_z;
 			}
 		}
 	}
@@ -125,19 +198,19 @@ UnloadMain(void) -> bool
 	int local_player_health_address = local_player + 0xEC;
 	int local_player_health =
 		*reinterpret_cast<int *>(local_player_health_address);
-	const int local_player_health_modified = 9999;
+	const int local_player_health_modified = HEALTH_MODIFIED;
 
 	int camera_address = local_player + 0x324;
 	int camera = *reinterpret_cast<int *>(camera_address);
-	const int camera_modified = 1852730990;
+	const int camera_modified = CAMERA_MODIFIED;
 
 	int debug_mode_free_camera_address = local_player + 0x74;
 	int debug_mode = *reinterpret_cast<int *>(debug_mode_free_camera_address);
-	const int debug_mode_modified = 262144;
+	const int debug_mode_modified = DEBUG_MODE_MODIFIED;
 
 	*reinterpret_cast<int *>(local_player_health_address) = 100;
-	*reinterpret_cast<int *>(camera_address) = 0;
-	*reinterpret_cast<int *>(debug_mode_free_camera_address) = 0;
+	*reinterpret_cast<int *>(camera_address) = NULL;
+	*reinterpret_cast<int *>(debug_mode_free_camera_address) = NULL;
 	kiero::shutdown();
 	return S_OK;
 }
@@ -148,7 +221,7 @@ RenderMain(void) -> void
 	ImGui::SetNextWindowPos(
 		{ImGui::GetIO().DisplaySize.x / 2, ImGui::GetIO().DisplaySize.y / 2},
 		ImGuiCond_Once, {0.5, 0.5});
-	ImGui::SetNextWindowSize({200.f, 100.f}, NULL);
+	ImGui::SetNextWindowSize({250.f, 150.f}, NULL);
 
 	if (::GetAsyncKeyState(VK_DELETE) & 1)
 		func.show_menu = !func.show_menu;
@@ -160,6 +233,10 @@ RenderMain(void) -> void
 		ImGui::Checkbox("Toggle teleport", &func.teleport);
 		ImGui::Checkbox("Funny camera", &func.funny_camera);
 		ImGui::Checkbox("Debug camera", &func.noclip);
+		ImGui::Checkbox("Show line", &func.line);
+		ImGui::SameLine();
+		ImGui::ColorEdit4("##line_color", reinterpret_cast<float *>(&col.line),
+						  ImGuiColorEditFlags_NoLabel);
 		if (ImGui::Button("Unload", {100, 50}))
 		{
 			UnloadMain();
